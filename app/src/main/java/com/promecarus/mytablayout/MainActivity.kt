975a432133fc2f0ec3fdbd21e4fb318808bbc566
promecarus <haikalslipi@gmail.com>
2023-09-06 21:37:49 +0700

    first commit

---
+++ app/src/main/java/com/promecarus/mytablayout/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.promecarus.mytablayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
